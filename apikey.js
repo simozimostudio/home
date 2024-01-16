ef400864ab3d376ac8631a89b3b1f0338f169f94
simozimostudio <156837511+simozimostudio@users.noreply.github.com>
2024-01-17 03:08:35 +0900

    Add files via upload

---
+++ apikey.js
@@ -0,0 +1,5 @@
+const config = {  
+    apikey : "AIzaSyCmQCFm1bfUmdpZmFPR36p3ReEGwV7A0-8" ,
+    postalgetkey : "AKfycbwp_lYxMAT3zKW0J8iGma53ZogIEWQUpGlrOjrBucZoCnVe8ivHfmSbuilqCch-rKY",
+    snsgetkey : "AKfycbypmDkRQ6_muh8iF9dNVA0q2ycBI-4IQXEKX7xbwf71_qlB8R9-9HUCvVoFTP0Ftek0tA"
+   }
